5e33e76f0098381a89302bf7dc9ff6f32f8a9a49
v-dhkon <v-dhkon@MAQ21243>
2017-10-23 23:30:24 +0530

    Adding tslint changes to GitHub

--- src/CustomVisuals/Published/RingChart/src/objectEnumerationUtility.ts
+++ src/CustomVisuals/Published/RingChart/src/objectEnumerationUtility.ts
@@ -10,9 +10,11 @@ module powerbi.extensibility.visual {
      */
     export function getValue<T>(objects: DataViewObjects, objectName: string, propertyName: string, defaultValue: T): T {
         if (objects) {
-            let object = objects[objectName];
+            let object: DataViewObject;
+            object = objects[objectName];
             if (object) {
-                let property: T = <T>object[propertyName];
+                let property: T;
+                property = <T>object[propertyName];
                 if (property !== undefined) {
                     return property;
                 }
@@ -32,14 +34,19 @@ module powerbi.extensibility.visual {
      * @param {string} propertyName             - Name of desired property.
      * @param {T} defaultValue                  - Default value of desired property.
      */
-    export function getCategoricalObjectValue<T>(category: DataViewCategoryColumn, index: number, objectName: string, propertyName: string, defaultValue: T): T {
-        let categoryObjects = category.objects;
+    export function getCategoricalObjectValue<T>(
+        category: DataViewCategoryColumn, index: number, objectName: string, propertyName: string, defaultValue: T): T {
+        let categoryObjects: DataViewObjects[];
+        categoryObjects = category.objects;
         if (categoryObjects) {
-            let categoryObject: DataViewObject = categoryObjects[index];
+            let categoryObject: DataViewObject;
+            categoryObject = categoryObjects[index];
             if (categoryObject) {
-                let object = categoryObject[objectName];
+                let object: DataViewPropertyValue;
+                object = categoryObject[objectName];
                 if (object) {
-                    let property: T = object[propertyName];
+                    let property: T;
+                    property = object[propertyName];
                     if (property !== undefined) {
                         return property;
                     }
