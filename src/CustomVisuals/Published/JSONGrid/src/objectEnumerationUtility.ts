99549afa241ea14c255bb1b7c5cb3d9fa65a3a08
Neekita Salvankar <Neekita Salvankar@MAQ40894>
2017-11-02 18:58:22 +0530

    Updated files for Grid visual

--- src/CustomVisuals/Published/JSONGrid/src/objectEnumerationUtility.ts
+++ src/CustomVisuals/Published/JSONGrid/src/objectEnumerationUtility.ts
@@ -8,16 +8,17 @@ module powerbi.extensibility.visual {
      * @param {string} propertyName     - Name of desired property.
      * @param {T} defaultValue          - Default value of desired property.
      */
-    export function getValue<T>(objects: DataViewObjects, objectName: string, propertyName: string, defaultValue: T ): T {
-        if(objects) {
-            let object = objects[objectName];
-            if(object) {
-                let property: T = <T>object[propertyName];
-                if(property !== undefined) {
+    export function getValue<T>(objects: DataViewObjects, objectName: string, propertyName: string, defaultValue: T): T {
+        if (objects) {
+            const object = objects[objectName];
+            if (object) {
+                const property: T = <T>object[propertyName];
+                if (property !== undefined) {
                     return property;
                 }
             }
         }
+
         return defaultValue;
     }
 
@@ -31,21 +32,23 @@ module powerbi.extensibility.visual {
      * @param {string} propertyName             - Name of desired property.
      * @param {T} defaultValue                  - Default value of desired property.
      */
-    export function getCategoricalObjectValue<T>(category: DataViewCategoryColumn, index: number, objectName: string, propertyName: string, defaultValue: T): T {
-        let categoryObjects = category.objects;
+    export function getCategoricalObjectValue<T>
+    (category: DataViewCategoryColumn, index: number, objectName: string, propertyName: string, defaultValue: T): T {
+        const categoryObjects = category.objects;
 
-        if(categoryObjects) {
-            let categoryObject: DataViewObject = categoryObjects[index];
-            if(categoryObject) {
-                let object = categoryObject[objectName];
-                if(object) {
-                    let property: T = <T>object[propertyName];
-                    if(property !== undefined) {
+        if (categoryObjects) {
+            const categoryObject: DataViewObject = categoryObjects[index];
+            if (categoryObject) {
+                const object = categoryObject[objectName];
+                if (object) {
+                    const property: T = <T>object[propertyName];
+                    if (property !== undefined) {
                         return property;
                     }
                 }
             }
         }
+
         return defaultValue;
     }
 }
--- src/CustomVisuals/Published/JSONGrid/src/visual.ts
+++ src/CustomVisuals/Published/JSONGrid/src/visual.ts
@@ -1,27 +1,63 @@
+/*
+ *  Power BI Visual CLI
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ''Software''), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+ interface IdefaultSetting {
+    fontSize: number;
+    maxRows: number;
+    sortKey: number;
+    sortOrder: string;
+    apiUrl: string;
+    isCustomRedirect: boolean;
+    columnNumber: number;
+}
 module powerbi.extensibility.visual {
 
-    export class Grid implements IVisual {
-        private svg: d3.Selection < SVGAElement > ;
-        private renderMAQVisual: any;
-        private xAxis: d3.Selection < SVGAElement > ;
-        public dataView: DataView;
-        private selectionManager: ISelectionManager;
-        public host: IVisualHost;
-        private target: HTMLElement;
-        private gridVisual: any;
-        private viewport: IViewport;
-        private settings: any;
-        private prevDataViewObjects: any = {};
+    import valueFormatter = powerbi.extensibility.utils.formatting.valueFormatter;
 
+    export class Grid implements IVisual {
+        public errorMsg: d3.Selection<SVGAElement>;
+        public dataView: DataView;
+        public host: IVisualHost;
+        private svg: d3.Selection<SVGAElement>;
+        private xAxis: d3.Selection<SVGAElement>;
+        private selectionManager: ISelectionManager;
+        private target: HTMLElement;
+        private gridVisual : void;
+        private viewport: IViewport;
+        private settings : IdefaultSetting;
+        private prevDataViewObjects : DataViewObjects;
 
         constructor(options: VisualConstructorOptions) {
             this.host = options.host;
             this.selectionManager = options.host.createSelectionManager();
-            this.viewport;
+            //this.viewport;
             this.target = options.element;
-           let svg = this.svg = d3.select(options.element).append('div')
+
+            this.svg = this.svg = d3.select(options.element).append('div')
                 .classed('DataDiv', true)
-                .attr("id", "KPIGrid");          
+                .attr('id', 'KPIGrid');
         }
 
         /**
@@ -32,21 +68,48 @@ module powerbi.extensibility.visual {
          *                                        and the dataView which contains all the data
          *                                        the visual had queried.
          */
-        public update(options: VisualUpdateOptions) {
-            var dataViews = options.dataViews;
-            if (!dataViews || 0 === dataViews.length ||  !dataViews[0].table) {
+        public update(options: VisualUpdateOptions) : VisualUpdateOptions {
+
+            let dataViews : DataView[];
+            dataViews  = options.dataViews;
+
+            if (!dataViews || 0 === dataViews.length || !dataViews[0].table) {
+               return;
+            }
+
+            const dataView: DataView = options.dataViews[0];
+            let flag: number = 0;
+            for (i = 0; i < dataView.table.columns.length; i++) {
+                if (dataView.table.columns[i].roles.hasOwnProperty('Values')) {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            d3.selectAll('#htmlChunk').remove();
+            if (!flag) {
+                this.svg.selectAll('*').remove();
+                const htmlChunk: string = 'Please select Values';
+                d3.select('.DataDiv').style('overflow', 'visible');
+                d3.select('.DataDiv')
+                    .append('div')
+                    .attr('id', 'htmlChunk')
+                    .style('margin-top', `${options.viewport.height / 2}px`)
+                    .style('margin-left', `10px`)
+                    .style('text-align', 'center')
+                    .style('font-size', `20px`)
+                    .text(htmlChunk);
+
                 return;
             }
-            var dataView = options.dataViews[0];
-            var dataView = options.dataViews[0];
-            var settingsChanged = this.getSettings(dataView.metadata.objects);
-            if (!this.gridVisual || settingsChanged || ((options.type & VisualUpdateType.Resize) || options.type & VisualUpdateType.ResizeEnd)) {
-                this.svg.selectAll("*").remove();
+            d3.select('.DataDiv').style('overflow', 'auto');
+            const settingsChanged: boolean = this.getSettings(dataView.metadata.objects);
+            if (!this.gridVisual || settingsChanged
+                || ((options.type && VisualUpdateType.Resize) || options.type && VisualUpdateType.ResizeEnd)) {
+                this.svg.selectAll('*').remove();
                 this.gridVisual = myMAQlibrary(dataView, this.settings);
             }
         }
-
-
         /**
          * Enumerates through the objects defined in the capabilities and adds the properties to the format pane
          *
@@ -55,11 +118,11 @@ module powerbi.extensibility.visual {
          */
         public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
 
-            let objectName = options.objectName;
-            let objectEnumeration: VisualObjectInstance[] = [];
+            const objectName: string = options.objectName;
+            const objectEnumeration: VisualObjectInstance[] = [];
 
             switch (objectName) {
-                case "GridConfig":
+                case 'GridConfig':
                     objectEnumeration.push({
                         objectName: options.objectName,
                         displayName: 'Font Updates',
@@ -75,7 +138,12 @@ module powerbi.extensibility.visual {
                         }
                     });
                     break;
-            };
+
+                    default:
+                    {
+                        break;
+                    }
+            }
 
             return objectEnumeration;
         }
@@ -85,20 +153,21 @@ module powerbi.extensibility.visual {
         }
 
         private getSettings(objects: DataViewObjects): boolean {
-            let settingsChanged = false;
-            if (typeof this.settings == 'undefined' || (JSON.stringify(objects) !== JSON.stringify(this.prevDataViewObjects))) {
+            let settingsChanged: boolean = false;
+            if (typeof this.settings === 'undefined' || (JSON.stringify(objects) !== JSON.stringify(this.prevDataViewObjects))) {
                 this.settings = {
-                    fontSize: getValue < number > (objects, 'GridConfig', 'fontSize', 12),
-                    maxRows: getValue < number > (objects, 'GridConfig', 'maxRows', 5),
-                    sortKey: getValue < number > (objects, 'GridConfig', 'sortKey', 1),
-                    sortOrder: getValue < string > (objects, 'GridConfig', 'sortOrder', 'ASC'),
-                    apiUrl: getValue < string > (objects, 'GridConfig', 'apiUrl', 'https://jsonplaceholder.typicode.com/posts/'),
-                    isCustomRedirect: getValue < boolean > (objects, 'GridConfig', 'isCustomRedirect', false),
-                    columnNumber: getValue < number > (objects, 'GridConfig', 'columnNumber', 1)
+                    fontSize: getValue<number>(objects, 'GridConfig', 'fontSize', 12),
+                    maxRows: getValue<number>(objects, 'GridConfig', 'maxRows', 5),
+                    sortKey: getValue<number>(objects, 'GridConfig', 'sortKey', 1),
+                    sortOrder: getValue<string>(objects, 'GridConfig', 'sortOrder', 'ASC'),
+                    apiUrl: getValue<string>(objects, 'GridConfig', 'apiUrl', 'https://jsonplaceholder.typicode.com/posts/'),
+                    isCustomRedirect: getValue<boolean>(objects, 'GridConfig', 'isCustomRedirect', false),
+                    columnNumber: getValue<number>(objects, 'GridConfig', 'columnNumber', 1)
                 };
                 settingsChanged = true;
             }
             this.prevDataViewObjects = objects;
+
             return settingsChanged;
         }
     }
